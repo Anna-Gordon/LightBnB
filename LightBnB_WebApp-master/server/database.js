24ef66afe302271aa616a9d0d412adc2b9c42909
Anna Gordon <annetta@email.com>
2020-03-06 00:36:07 +0000

    Add property function for Create  Listing

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -100,12 +100,8 @@ const getAllReservations = function(guest_id, limit = 10) {
       [guest_id, limit]
     )
     .then(res => res.rows);
-
-  // return getAllProperties(null, 2);
 }
-// 45 | Louis Washington | chloecarter@google.com | $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
 
-// console.log(getAllReservations(45, 2));
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -117,8 +113,6 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-// return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
-// .then(res => res.rows);
 const getAllProperties = (options, limit = 10) => {
     // 1
     const queryParams = [];
@@ -186,9 +180,41 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`INSERT INTO properties (
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, 
+  [property.owner_id, 
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms ])
+    .then(res => {
+      console.log(res.rows)
+      return res.rows;
+    })
+    .catch(err => {
+      console.log('Error: ', err);
+    })
 }
 exports.addProperty = addProperty;
